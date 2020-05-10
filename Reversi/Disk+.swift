f95f569d6e170b7ca1d63bbca250191c2c3955b1
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2020-05-10 20:25:31 +0900

    DataTypeに分離

---
+++ Reversi/Disk+.swift
@@ -0,0 +1,61 @@
+//
+//  Disk+.swift
+//  Reversi
+//
+//  Created by ucuc on 2020/05/10.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+import Foundation
+
+extension Disk {
+    init(index: Int) {
+        for side in Disk.sides {
+            if index == side.index {
+                self = side
+                return
+            }
+        }
+        preconditionFailure("Illegal index: \(index)")
+    }
+    
+    var index: Int {
+        switch self {
+        case .dark: return 0
+        case .light: return 1
+        }
+    }
+}
+
+extension Optional where Wrapped == Disk {
+    init?<S: StringProtocol>(symbol: S) {
+        switch symbol {
+        case "x":
+            self = .some(.dark)
+        case "o":
+            self = .some(.light)
+        case "-":
+            self = .none
+        default:
+            return nil
+        }
+    }
+    
+    var symbol: String {
+        switch self {
+        case .some(.dark):
+            return "x"
+        case .some(.light):
+            return "o"
+        case .none:
+            return "-"
+        }
+    }
+}
+
+struct DiskPlacementError: Error {
+    let disk: Disk
+    let x: Int
+    let y: Int
+}
+
---
+++ Reversi/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Reversi
+//
+//  Created by ucuc on 2020/05/10.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+import Foundation
+
+enum Player: Int {
+    case manual = 0
+    case computer = 1
+}
